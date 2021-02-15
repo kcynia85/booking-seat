ee17dabaa7f2bc9a55ef3c6bb0f143e472efb34c
const.kamil.lazarski@gmail.com <const.kamil.lazarski@gmail.com>
2021-02-15 20:40:51 +0100

    added populating data from localStorage for seats and movie

--- app.js
+++ app.js
@@ -14,15 +14,33 @@
 
   const updateSelectedCount = () => {
     const selectedSeats = document.querySelectorAll('.cinema__seat.cinema__seat--selected');
-    const selectedSeatsCount = selectedSeats.length; 
-
     const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
-    
+  
     localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
     
+    const selectedSeatsCount = selectedSeats.length; 
+
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice; 
 
+  };
+
+  const populateUI = () => {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+      seats.forEach((seat, index) => {
+        if (selectedSeats.indexOf(index) > -1) {
+          seat.classList.add('cinema__seat--selected')
+        }
+      });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if(selectedMovieIndex !== null) {
+      movieSelect.selectedIndex = selectedMovieIndex;
+    }
   };
 
   movieSelect.addEventListener('change', e => {
@@ -32,7 +50,7 @@
     updateSelectedCount();
   });
 
-   cinemaHall.addEventListener('click', e => {
+  cinemaHall.addEventListener('click', e => {
     if (e.target.classList.contains('cinema__seat') 
         && !e.target.classList.contains('cinema__seat--occupied')) {
       e.target.classList.toggle('cinema__seat--selected');
@@ -40,6 +58,10 @@
       updateSelectedCount();
     };
    });
-
+   
+   populateUI();
 };
+
+
+
 
